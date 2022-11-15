31175aea95ffd478e982a9bf0154d880f24707b2
SohailLuffy <sohailislammail97@gmail.com>
2022-11-15 21:20:38 +0530

    Account Balance

---
+++ Encapsulation/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecapsulation
+{
+    class Account
+    {
+        int AccountBalance = 1000;
+        public int Balance
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("You cannot pass");
+                }
+                else
+                {
+
+                    AccountBalance = value;
+                }
+            }
+            get
+            {
+                return AccountBalance;
+            }
+        }
+
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Principal;
+
+namespace Ecapsulation
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+            myAccount.Balance = 10000;
+            Console.WriteLine("Your account balance is:" + myAccount.Balance);
+
+            Console.ReadLine();
+
+        }
+    }
+}
